5c2f9d0ab063c0d0696da806133a18ed364d27d0
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-06 20:19:24 +0900

    init

--- main.go
+++ main.go
@@ -1,12 +1,1 @@
 package main
-
-import (
-	"github.com/3boku/Golang-JWT/middleware"
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-
-	r.Use(middleware.RedisMiddleware())
-}
--- utils/user.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-type User struct {
-	ID       uint64 `json:"id"`
-	Email    string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
